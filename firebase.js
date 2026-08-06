104a4cf62f0a434b95e6bcf69a3d312962aad4aa
vishalsonvani16125112-code <vishalsonvani16125112@gmail.com>
2026-08-06 16:12:45 +0530

    Remove script tags from firebase.js

--- firebase.js
+++ firebase.js
@@ -1,5 +1,4 @@
-<script type="module">
-  // Import the functions you need from the SDKs you need
+ // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/12.17.1/firebase-app.js";
   import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.17.1/firebase-analytics.js";
   // TODO: Add SDKs for Firebase products that you want to use
@@ -20,4 +19,4 @@
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
   const analytics = getAnalytics(app);
-</script>
+
